5216417a51271321aac56c5ca8d881300917a0f4
vdmivchenko <vdmivchenko.lt@gmail.com>
2020-09-10 14:07:08 +0300

    the controlled component is configured to search

--- src/components/common/Search/Search.tsx
+++ src/components/common/Search/Search.tsx
@@ -1,55 +1,57 @@
 import React, { useCallback } from "react";
 import { BsSearch } from "react-icons/bs";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { debounce } from "debounce";
 
 import {
-    resetSearchMovies,
-    getSearchMoviesRequest,
-    setSearchValue,
+  resetSearchMovies,
+  getSearchMoviesRequest,
+  setSearchValue,
+  setSelectedPage,
 } from "../../../store/actions";
 import styles from "./Search.module.scss";
+import { selectSearchMovies } from "../../../store/selectors";
 
 type Props = {
-    placeholder: string;
-    title: string;
-    type: string;
+  placeholder: string;
+  title: string;
+  type: string;
 };
 
-const Search: React.FC<Props> = ({
-    placeholder,
-    title,
-    type,
-}) => {
-    const dispatch = useDispatch();
+const Search: React.FC<Props> = ({ placeholder, title, type }) => {
+  const dispatch = useDispatch();
 
-    const onChangeDispatch = useCallback(
-        (value: string, page: number) => {
-            if (!value) return dispatch(resetSearchMovies());
-            dispatch(getSearchMoviesRequest(value, page));
-            dispatch(setSearchValue(value));
-        },
-        [dispatch]
-    );
+  const { searchValue } = useSelector(selectSearchMovies);
 
-    const debounceChange = debounce(onChangeDispatch, 500);
+  const onChangeDispatch = useCallback(
+    (value: string, page: number) => {
+      if (!value) return dispatch(resetSearchMovies());
+      dispatch(getSearchMoviesRequest(value, page));
+      dispatch(setSelectedPage(0));
+    },
+    [dispatch]
+  );
 
-    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        debounceChange(e.target.value, 1);
-    };
+  const debounceChange = debounce(onChangeDispatch, 500);
 
-    return (
-        <div className={styles.bar}>
-            <BsSearch className={styles.searchIcon} />
-            <input
-                className={styles.searchbar}
-                placeholder={placeholder}
-                type={type}
-                title={title}
-                onChange={handleSearchChange}
-            />
-        </div>
-    );
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    debounceChange(e.target.value, 1);
+    dispatch(setSearchValue(e.target.value));
+  };
+
+  return (
+    <div className={styles.bar}>
+      <BsSearch className={styles.searchIcon} />
+      <input
+        className={styles.searchbar}
+        placeholder={placeholder}
+        type={type}
+        title={title}
+        value={searchValue}
+        onChange={handleSearchChange}
+      />
+    </div>
+  );
 };
 
 export default Search;
