1b0524b5a8b4b9e92867f9ed5f00a68f4d9ba3ed
vdmivchenko <vdmivchenko.lt@gmail.com>
2020-09-10 13:58:26 +0300

    added jest and enzyme

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,16 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+
+// import '@testing-library/jest-dom/extend-expect';
+
+import Enzyme from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import { createSerializer } from "enzyme-to-json";
+
+//@ts-ignore
+expect.addSnapshotSerializer(createSerializer({ mode: "deep", noKey: true }));
+Enzyme.configure({
+  adapter: new EnzymeAdapter(),
+  disableLifecycleMethods: true,
+});
