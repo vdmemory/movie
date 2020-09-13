908be585f94d1d0d2b3deed6fc5695133f82ebf8
vdmivchenko <vdmivchenko.lt@gmail.com>
2020-09-13 09:47:29 +0300

    testting reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -4,113 +4,113 @@ import { DetailsMovie } from "../api/detailsMovie";
 import { SearchMovies } from "../api/searchMovies";
 import { Actions } from "./actions";
 
-const initialState = {
-    searchMovies: {} as Record<string, string | SearchMovies[]>,
-    detailsMovie: {} as DetailsMovie,
-    loading: {
-        searchMovies: false as boolean,
-        detailsMovie: false as boolean,
-    },
-    error: {
-        searchMovies: null as string | null,
-        detailsMovie: null as string | null,
-    },
-    searchValue: "" as string,
-    selectedPage: 0 as number
+export const initialState = {
+  searchMovies: {} as Record<string, string | SearchMovies[]>,
+  detailsMovie: {} as DetailsMovie,
+  loading: {
+    searchMovies: false as boolean,
+    detailsMovie: false as boolean,
+  },
+  error: {
+    searchMovies: null as string | null,
+    detailsMovie: null as string | null,
+  },
+  searchValue: "" as string,
+  selectedPage: 0 as number,
 };
 
 export type State = typeof initialState;
 
 const reducer: Reducer<State, Actions> = (state = initialState, action) => {
-    switch (action.type) {
-        case type.GET_SEARCH_MOVIES_REQUEST:
-            return {
-                ...state,
-                loading: {
-                    ...state.loading,
-                    searchMovies: true,
-                },
-                error: {
-                    ...state.error,
-                    searchMovies: null,
-                },
-            };
-        case type.GET_SEARCH_MOVIES_SUCCESS:
-            return {
-                ...state,
-                loading: {
-                    ...state.loading,
-                    searchMovies: false,
-                },
-                searchMovies: action.payload,
-            };
-        case type.GET_SEARCH_MOVIES_FAILURE:
-            return {
-                ...state,
-                loading: {
-                    ...state.loading,
-                    searchMovies: false,
-                },
-                error: {
-                    ...state.error,
-                    searchMovies: action.payload,
-                },
-            };
-        case type.GET_DETAILS_MOVIE_REQUEST:
-            return {
-                ...state,
-                loading: {
-                    ...state.loading,
-                    detailsMovie: true,
-                },
-                error: {
-                    ...state.error,
-                    detailsMovie: null,
-                },
-            };
-        case type.GET_DETAILS_MOVIE_SUCCESS:
-            return {
-                ...state,
-                loading: {
-                    ...state.loading,
-                    detailsMovie: false,
-                },
-                detailsMovie: action.payload,
-            };
-        case type.GET_DETAILS_MOVIE_FAILURE:
-            return {
-                ...state,
-                loading: {
-                    ...state.loading,
-                    detailsMovie: false,
-                },
-                error: {
-                    ...state.error,
-                    detailsMovie: action.payload,
-                },
-            };
-        case type.RESET_SEARCH_MOVIES:
-            return {
-                ...state,
-                searchMovies: {} as Record<string, string | SearchMovies[]>,
-                error: {
-                    ...state.error,
-                    searchMovies: null,
-                },
-            };
-        case type.SET_SEARCH_VALUE:
-            return {
-                ...state,
-                searchValue: action.payload,
-            };
-        case type.SET_SELECTED_PAGE:
-            return {
-                ...state,
-                selectedPage: action.payload,
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case type.GET_SEARCH_MOVIES_REQUEST:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          searchMovies: true,
+        },
+        error: {
+          ...state.error,
+          searchMovies: null,
+        },
+      };
+    case type.GET_SEARCH_MOVIES_SUCCESS:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          searchMovies: false,
+        },
+        searchMovies: action.payload,
+      };
+    case type.GET_SEARCH_MOVIES_FAILURE:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          searchMovies: false,
+        },
+        error: {
+          ...state.error,
+          searchMovies: action.payload,
+        },
+      };
+    case type.GET_DETAILS_MOVIE_REQUEST:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          detailsMovie: true,
+        },
+        error: {
+          ...state.error,
+          detailsMovie: null,
+        },
+      };
+    case type.GET_DETAILS_MOVIE_SUCCESS:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          detailsMovie: false,
+        },
+        detailsMovie: action.payload,
+      };
+    case type.GET_DETAILS_MOVIE_FAILURE:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          detailsMovie: false,
+        },
+        error: {
+          ...state.error,
+          detailsMovie: action.payload,
+        },
+      };
+    case type.RESET_SEARCH_MOVIES:
+      return {
+        ...state,
+        searchMovies: {} as Record<string, string | SearchMovies[]>,
+        error: {
+          ...state.error,
+          searchMovies: null,
+        },
+      };
+    case type.SET_SEARCH_VALUE:
+      return {
+        ...state,
+        searchValue: action.payload,
+      };
+    case type.SET_SELECTED_PAGE:
+      return {
+        ...state,
+        selectedPage: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
--- src/store/tests/reducer.test.ts
+++ src/store/tests/reducer.test.ts
@@ -3,33 +3,208 @@ import reducer from "../reducer";
 import * as actions from "../actions";
 import { Actions } from "../actions";
 
-
 import { DetailsMovie } from "../../api/detailsMovie";
 import { SearchMovies } from "../../api/searchMovies";
 
 const initialResult = {
-    searchMovies: {} as Record<string, string | SearchMovies[]>,
-    detailsMovie: {} as DetailsMovie,
-    loading: {
-        searchMovies: false as boolean,
-        detailsMovie: false as boolean,
-    },
-    error: {
-        searchMovies: null as string | null,
-        detailsMovie: null as string | null,
-    },
-    searchValue: "" as string,
-    selectedPage: 0 as number,
+  searchMovies: {} as Record<string, string | SearchMovies[]>,
+  detailsMovie: {} as DetailsMovie,
+  loading: {
+    searchMovies: false as boolean,
+    detailsMovie: false as boolean,
+  },
+  error: {
+    searchMovies: null as string | null,
+    detailsMovie: null as string | null,
+  },
+  searchValue: "" as string,
+  selectedPage: 0 as number,
 };
 
-describe('Reducer', () => {
+const searchMovies: SearchMovies[] = [
+  {
+    title: "this title",
+    year: "this year",
+    imdbId: "this imdbId;",
+    type: "this type",
+    poster: "this poster",
+  },
+];
 
-    test("should handle initial state", () => {
-        expect(reducer(undefined, {} as Actions)).toEqual(initialResult);
+const detailsMovie: DetailsMovie = {
+  title: "string",
+  year: "this year",
+  runtime: "string",
+  genre: "string",
+  director: "string",
+  actors: "string",
+  plot: "string",
+  language: "string",
+  country: "string",
+  poster: "string",
+  imdbRating: "string",
+  imdbId: "string",
+  type: "string",
+  response: true,
+  error: "string",
+};
+
+describe("Reducer", () => {
+  test("should handle initial state", () => {
+    expect(reducer(undefined, {} as Actions)).toEqual(initialResult);
+  });
+
+  describe("SearchMovies reducer", () => {
+    test("Should handle GET_SEARCH_MOVIES", () => {
+      expect(
+        reducer(undefined, {
+          type: types.GET_SEARCH_MOVIES_REQUEST,
+          payload: { value: "value", page: 1 },
+        } as Actions)
+      ).toEqual({
+        ...initialResult,
+        loading: {
+          ...initialResult.loading,
+          searchMovies: true,
+        },
+      });
+
+      expect(
+        reducer(undefined, {
+          type: types.GET_SEARCH_MOVIES_SUCCESS,
+          payload: { value: "value" },
+        } as Actions)
+      ).toEqual({
+        ...initialResult,
+        loading: {
+          ...initialResult.loading,
+          searchMovies: false,
+        },
+        searchMovies: {
+          value: "value",
+        },
+      });
+
+      expect(
+        reducer(undefined, {
+          type: types.GET_SEARCH_MOVIES_SUCCESS,
+          payload: { searchMovies },
+        } as Actions)
+      ).toEqual({
+        ...initialResult,
+        loading: {
+          ...initialResult.loading,
+          searchMovies: false,
+        },
+        searchMovies: { searchMovies },
+      });
+
+      expect(
+        reducer(undefined, {
+          type: types.GET_SEARCH_MOVIES_FAILURE,
+          payload: "error message",
+        } as Actions)
+      ).toEqual({
+        ...initialResult,
+        loading: {
+          ...initialResult.loading,
+          searchMovies: false,
+        },
+        error: {
+          ...initialResult.error,
+          searchMovies: "error message",
+        },
+      });
     });
+  });
+  describe("DetailsMovie reducer", () => {
+    test("Should handle GET_DETAILS_MOVIE_REQUEST", () => {
+      expect(
+        reducer(undefined, {
+          type: types.GET_DETAILS_MOVIE_REQUEST,
+          payload: "movieId",
+        } as Actions)
+      ).toEqual({
+        ...initialResult,
+        loading: {
+          ...initialResult.loading,
+          detailsMovie: true,
+        },
+        error: {
+          ...initialResult.error,
+          detailsMovie: null,
+        },
+      });
 
-    describe("SearchMovies reducer", () => {
+      expect(
+        reducer(undefined, {
+          type: types.GET_DETAILS_MOVIE_SUCCESS,
+          payload: detailsMovie,
+        } as Actions)
+      ).toEqual({
+        ...initialResult,
+        loading: {
+          ...initialResult.loading,
+          detailsMovie: false,
+        },
+        detailsMovie,
+      });
 
+      expect(
+        reducer(undefined, {
+          type: types.GET_DETAILS_MOVIE_FAILURE,
+          payload: "error message",
+        } as Actions)
+      ).toEqual({
+        ...initialResult,
+        loading: {
+          ...initialResult.loading,
+          detailsMovie: false,
+        },
+        error: {
+          ...initialResult.error,
+          detailsMovie: "error message",
+        },
+      });
     });
-    describe("DetailsMovie reducer", () => {});
+  });
+
+  test("Should handle RESET_SEARCH_MOVIES", () => {
+    expect(
+      reducer(undefined, {
+        type: types.RESET_SEARCH_MOVIES,
+      } as Actions)
+    ).toEqual({
+      ...initialResult,
+      searchMovies: {} as Record<string, string | SearchMovies[]>,
+      error: {
+        ...initialResult.error,
+        searchMovies: null,
+      },
+    });
+  });
+
+  test("Should handle SET_SEARCH_VALUE", () => {
+    expect(
+      reducer(undefined, {
+        type: types.SET_SEARCH_VALUE,
+        payload: "search value",
+      } as Actions)
+    ).toEqual({
+      ...initialResult,
+      searchValue: "search value",
+    });
+  });
+
+  test("Should handle SET_SELECTED_PAGE", () => {
+    expect(
+      reducer(undefined, {
+        type: types.SET_SELECTED_PAGE,
+        payload: 1,
+      } as Actions)
+    ).toEqual({
+      ...initialResult,
+      selectedPage: 1,
+    });
+  });
 });
